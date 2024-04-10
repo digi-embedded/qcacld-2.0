769f380f6b3d755600ffdfb2fce9bc7e2a1df117
Isaac Hermida <isaac.hermida@digi.com>
2024-04-10 12:34:39 +0200

    replace PDE_DATA with pde_data
    https://onedigi.atlassian.net/browse/DEL-9007
    
    Signed-off-by: Isaac Hermida <isaac.hermida@digi.com>

--- CORE/HDD/src/wlan_hdd_memdump.c
+++ CORE/HDD/src/wlan_hdd_memdump.c
@@ -68,7 +68,7 @@ static void *memdump_get_file_data(struct file *file)
 {
 	void *hdd_ctx;
 
-	hdd_ctx = PDE_DATA(file_inode(file));
+	hdd_ctx = pde_data(file_inode(file));
 	return hdd_ctx;
 }
 #else
--- CORE/SERVICES/HIF/ath_procfs.c
+++ CORE/SERVICES/HIF/ath_procfs.c
@@ -65,11 +65,11 @@ static void *get_hif_hdl_from_file(struct file *file)
 #endif
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(3,10,0))
 #if defined(HIF_PCI)
-	scn = (struct hif_pci_softc *)PDE_DATA(file_inode(file));
+	scn = (struct hif_pci_softc *)pde_data(file_inode(file));
 #elif defined(HIF_USB)
-	scn = (struct hif_usb_softc *)PDE_DATA(file_inode(file));
+	scn = (struct hif_usb_softc *)pde_data(file_inode(file));
 #elif defined(HIF_SDIO)
-	scn = (struct ath_hif_sdio_softc *)PDE_DATA(file_inode(file));
+	scn = (struct ath_hif_sdio_softc *)pde_data(file_inode(file));
 #endif
 #else
 #if defined(HIF_PCI)
