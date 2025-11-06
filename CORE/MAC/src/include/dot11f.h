b8fbec65875653369614d24dfdf1cceb0a2fd505
Arturo Buzarra <arturo.buzarra@digi.com>
2025-11-06 09:06:38 +0100

    qcacld-2.0: Support more Cipher / Akm suits in RSN IE parsing
    propagation from qcacld-3.0 to qcacld-2.0
    
    Currently, while parsing RSN IE host allow up to 4 max
    cipher / akm suits. Hence if beacon / probe response
    carry more than 4 cipher/akm suits then RSNIE parsing
    get fails.
    As a part of fix, increase cipher/akm max count to 6.
    
    Change-Id: I62f8797b480f0cc82586f76ec7fd318499bc7628
    CRs-Fixed: 2081030
    
    https://onedigi.atlassian.net/browse/DEL-9825
    
    (backport from qcacld-3.0 commit 9f1345284e48f066169f8a599401fca930469402)
    Signed-off-by: Arturo Buzarra <arturo.buzarra@digi.com>

--- CORE/MAC/src/include/dot11f.h
+++ CORE/MAC/src/include/dot11f.h
@@ -4593,9 +4593,9 @@ typedef struct sDot11fIERSN {
     tANI_U16     version /* Must be 1! */;
     tANI_U8      gp_cipher_suite[4];
     tANI_U16     pwise_cipher_suite_count;
-    tANI_U8      pwise_cipher_suites[4][4];
+    tANI_U8      pwise_cipher_suites[6][4];
     tANI_U16     akm_suite_count;
-    tANI_U8      akm_suites[4][4];
+    tANI_U8      akm_suites[6][4];
     tANI_U8      RSN_Cap[2];
     tANI_U16     pmkid_count;
     tANI_U8      pmkid[4][16];
@@ -4607,7 +4607,7 @@ typedef struct sDot11fIERSN {
 // N.B. These #defines do *not* include the EID & length
 #define DOT11F_IE_RSN_MIN_LEN ( 6 )
 
-#define DOT11F_IE_RSN_MAX_LEN ( 114 )
+#define DOT11F_IE_RSN_MAX_LEN ( 130 )
 
 #ifdef __cplusplus
 extern "C" {
