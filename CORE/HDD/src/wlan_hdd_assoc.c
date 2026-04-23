c392fafbdbc595b690a8c0fa4ab2221202ae1ad4
Mike Engel <Mike.Engel@digi.com>
2026-04-23 13:34:31 +0200

    hdd: wlan: fix compile error with new kernel patch level
    This commit fixes the following compile error using the updated
    kernel patch level.
    
    error: invalid application of 'sizeof' to incomplete type 'char[]'
    memset(wrqu->ap_addr.sa_data,0,sizeof(wrqu->ap_addr.sa_data));
    
    Now sa_data has a dynamic size and that cannot be handled by sizeof
    during compile time.
    
    Signed-off-by: Mike Engel <Mike.Engel@digi.com>

--- CORE/HDD/src/wlan_hdd_assoc.c
+++ CORE/HDD/src/wlan_hdd_assoc.c
@@ -6473,7 +6473,11 @@ static int __iw_get_ap_address(struct net_device *dev,
     }
     else
     {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 5, 150)
+        memset(wrqu->ap_addr.sa_data,0,sizeof(wrqu->ap_addr.sa_data_min));
+#else
         memset(wrqu->ap_addr.sa_data,0,sizeof(wrqu->ap_addr.sa_data));
+#endif
     }
     EXIT();
     return 0;
